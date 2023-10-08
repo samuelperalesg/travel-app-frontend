4e101e68911730e5f8a840618ea68681e66400df
samuelperalesg <91507521+samuelperalesg@users.noreply.github.com>
2023-10-07 19:35:42 -0600

    wip: footer - edit styling and links

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,13 +4,20 @@ function Footer() {
   return (
     <div id="footerContainer">
       <div id="footerHeader">
-        <div id="logoAndName">
-          <img src="https://i.imgur.com/950J1fr_d.webp?maxwidth=760&fidelity=grand" id="DumbbellImgFooter" alt=""/>
-          <h1 id="footerHeader">WORLD TRAVELER</h1>
+        <div id="leftColumn">
+          <div id="logoAndName">
+            <h1>WORLD TRAVELER</h1>
+          </div>
+          <div id="socialLinks">
+            <a href="https://www.linkedin.com/in/samuelperalesg" target="_blank" rel="noopener noreferrer">
+              <img src="https://i.imgur.com/0hmNIqq_d.webp?maxwidth=760&fidelity=grand" id="linkedinImg" alt="LinkedIn"/>
+            </a>
+            <a href="https://github.com/samuelperalesg/travel-app-frontend/" target="_blank" rel="noopener noreferrer">
+              <img src="https://i.imgur.com/q4EQ4Me_d.webp?maxwidth=760&fidelity=grand" id="gitHubImg" alt="GitHub"/>
+            </a>
+          </div>
         </div>
-        <img src="https://i.imgur.com/0hmNIqq_d.webp?maxwidth=760&fidelity=grand" id="linkedinImg" alt=""/>
-        <img src="https://i.imgur.com/q4EQ4Me_d.webp?maxwidth=760&fidelity=grand" id="gitHubImg" alt=""/>
-        <p id="copyrightFooter">@2022 WorldTraveler, M.O. All Rights Reserved.</p>
+        <p id="copyrightFooter">@2023 WorldTraveler</p>
       </div>
     </div>
   )
